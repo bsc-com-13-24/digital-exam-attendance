5aff4e4d3517cdcca6d585350c88038fc3af8e97
bsc-com-02-24 <bsc-com-02-24@unima.ac.mw>
2026-04-11 13:46:20 +0100

    chore: add ConfigModule to AppModule to enable .env file to be read

--- digital-exam-attendance/src/app.module.ts
+++ digital-exam-attendance/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SessionModule } from './session/session.module';
 import { OfflineModule } from './offline/offline.module';
 
 @Module({
-  imports: [SessionModule, OfflineModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}),SessionModule, OfflineModule],
   controllers: [AppController],
   providers: [AppService],
 })
