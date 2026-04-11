38f5eb2cc34a6e391791edd0b0c27222adbd9f25
bsc-com-02-24 <bsc-com-02-24@unima.ac.mw>
2026-04-11 13:04:57 +0100

    chore: initial NEstJS project scaffold with offline and session modules

---
+++ digital-exam-attendance/src/offline/offline.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { OfflineService } from './offline.service';
+import { OfflineController } from './offline.controller';
+
+@Module({
+  providers: [OfflineService],
+  controllers: [OfflineController]
+})
+export class OfflineModule {}
