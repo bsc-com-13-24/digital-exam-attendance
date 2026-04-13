c3496a67a9cc161cc2d53cd3494162f30101de29
bsc-com-23-24 <bsc-com-23-24@unima.ac.mw>
2026-04-13 12:50:09 +0200

    Remove attendance.module.ts

--- src/attendance/attendance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AttendanceService } from './attendance.service';
-import { AttendanceController } from './attendance.controller';
-
-@Module({
-  providers: [AttendanceService],
-  controllers: [AttendanceController]
-})
-export class AttendanceModule {}
