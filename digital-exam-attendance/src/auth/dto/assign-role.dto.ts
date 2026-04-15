133fa4bb72696cb928e7b9579d64a11e9ace854f
ALLAN <nkhudzuleallan@gmail.com>
2026-04-15 14:46:39 +0200

    added user dto

--- digital-exam-attendance/src/auth/dto/assign-role.dto.ts
+++ digital-exam-attendance/src/auth/dto/assign-role.dto.ts
@@ -1,11 +1,11 @@
-import { IsString, IsNotEmpty, IsUUID } from 'class-validator';
+import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
 
-export class AssignRoleDto {
-    @IsUUID('4', { message: 'User ID must be a valid UUID' })
-    @IsNotEmpty({ message: 'User ID is required' })
-    userId!: string;
+export class AssignRoleDto{
+    @IsString()
+    @IsNotEmpty()
+    user_id!: string;
 
     @IsString()
-    @IsNotEmpty({ message: 'Role name is required' })
-    roleName!: string;
+    @IsNotEmpty()
+    role_id!: string;
 }
