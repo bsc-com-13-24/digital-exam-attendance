48ca241ae4708cca919ebe9e3c518877f2c784f5
Allan Nkhudzule <bsc-com-13-24@unima.ac.mw>
2026-04-19 09:57:31 +0200

    feat: implement authentication with JWT, including user registration, login, and profile management

--- digital-exam-attendance/src/auth/dto/create-user.dto.ts
+++ digital-exam-attendance/src/auth/dto/create-user.dto.ts
@@ -1,23 +1,24 @@
-import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+// dto/create-user.dto.ts
+import { IsString, IsOptional, IsNotEmpty, IsEmail } from 'class-validator';
 
-export class CreateUserDto{
-    @IsString()
-    @IsNotEmpty()
-    first_name!: string;
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  first_name!: string;
 
-    @IsString()
-    @IsNotEmpty()
-    last_name!: string;
-    
-    @IsString()
-    @IsNotEmpty()
-    email! : string;
+  @IsString()
+  @IsNotEmpty()
+  last_name!: string;
 
-    @IsString()
-    @IsNotEmpty()
-    password! : string;
+  @IsEmail()                 
+  @IsNotEmpty()
+  email!: string;
 
-    @IsString()
-    @IsOptional()
-    role! : string;
+  @IsString()
+  @IsNotEmpty()
+  password!: string;          
+
+  @IsString()
+  @IsOptional()
+  role!: string;
 }
