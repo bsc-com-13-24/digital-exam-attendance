eaf547ff1eaf0ebc376e99042e7f31393b1a59d8
bsc-com-23-24 <bsc-com-23-24@unima.ac.mw>
2026-05-07 07:48:01 +0200

    feat: implement DTOs for authentication, attendance, and sessions, and define the Room entity

--- digital-exam-attendance/src/auth/dto/change-password.dto.ts
+++ digital-exam-attendance/src/auth/dto/change-password.dto.ts
@@ -1,15 +1,19 @@
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChangePasswordDto {
+    @ApiProperty({ example: 'old_password', description: 'Old Password' })
     @IsString()
     @IsNotEmpty()
     old_password!: string;
 
+    @ApiProperty({ example: 'new_password', description: 'New Password' })
     @IsString()
     @IsNotEmpty()
     @MinLength(4, { message: 'New password must be at least 4 characters long' })
     new_password!: string;
 
+    @ApiProperty({ example: 'confirm_password', description: 'Confirm Password' })
     @IsString()
     @IsNotEmpty()
     @MinLength(4, { message: 'Confirm password must be at least 4 characters long' })
--- digital-exam-attendance/src/rooms/dto/create-room.dto.ts
+++ digital-exam-attendance/src/rooms/dto/create-room.dto.ts
@@ -1,25 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsInt, IsBoolean, IsOptional, MaxLength, Min } from 'class-validator';
 
 export class CreateRoomDto {
+  @ApiProperty({ example: 'A101' })
   @IsString()
   @IsNotEmpty()
   @MaxLength(20)
   room_code!: string;
 
+  @ApiProperty({ example: 'Room A', description: 'Room Name' })
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
   name!: string;
 
+  @ApiProperty({ example: 'Building A', description: 'Building Name' })
   @IsString()
   @IsOptional()
   @MaxLength(255)
   building?: string;
 
+  @ApiProperty({ example: 100, description: 'Capacity of the room' })
   @IsInt()
   @Min(1)
   capacity!: number;
 
+  @ApiProperty({ example: true, description: 'Is the room active?' })
   @IsBoolean()
   @IsOptional()
   is_active?: boolean;
