f03c3798e10769c0a761d8dc80b345b799493b28
ALLAN <nkhudzuleallan@gmail.com>
2026-05-09 00:01:02 +0200

    feat: add OpenAPI documentation decorators to session and attendance DTOs

--- digital-exam-attendance/src/session/dto/enroll-students.dto.ts
+++ digital-exam-attendance/src/session/dto/enroll-students.dto.ts
@@ -1,8 +1,10 @@
 import { IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 import { AddStudentDto } from './add-student.dto';
 
 export class EnrollStudentsDto {
+  @ApiProperty({ type: [AddStudentDto], description: 'List of students to enroll' })
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => AddStudentDto)
