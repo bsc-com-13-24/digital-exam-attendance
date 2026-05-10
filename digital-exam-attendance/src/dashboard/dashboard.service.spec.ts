88c08763dde242b1f5ee015d7c1efd4818700a72
Allan Nkhudzule <bsc-com-13-24@unima.ac.mw>
2026-05-10 05:34:24 +0200

    update unit test dependency configurations and remove obsolete app spec files

--- digital-exam-attendance/src/dashboard/dashboard.service.spec.ts
+++ digital-exam-attendance/src/dashboard/dashboard.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardService } from './dashboard.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Session } from '../session/entities/sessions.entity';
+import { AttendanceRecord } from '../attendance/entities/attendance-records.entity';
+import { SessionStudent } from '../session/entities/session-students.entity';
 
 describe('DashboardService', () => {
   let service: DashboardService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DashboardService],
+      providers: [
+        DashboardService,
+        {
+          provide: getRepositoryToken(Session),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(AttendanceRecord),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(SessionStudent),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<DashboardService>(DashboardService);
--- digital-exam-attendance/src/rooms/rooms.service.spec.ts
+++ digital-exam-attendance/src/rooms/rooms.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomsService } from './rooms.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Room } from './entities/rooms.entity';
+import { SessionStudent } from '../session/entities/session-students.entity';
+import { AttendanceRecord } from '../attendance/entities/attendance-records.entity';
 
 describe('RoomsService', () => {
   let service: RoomsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomsService],
+      providers: [
+        RoomsService,
+        {
+          provide: getRepositoryToken(Room),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(SessionStudent),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(AttendanceRecord),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<RoomsService>(RoomsService);
